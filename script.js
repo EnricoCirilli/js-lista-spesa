5606ab72b53e6e0fdf3a51dc62b78253b4f8a621
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-25 20:05:08 +0200

    add: event listener test

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-
-/*const listaSpesa = [ "marmellata", "pane", "burro", "caffè"];
-
+const listaSpesa = [ "marmellata", "pane", "burro", "caffè"];
 
 let i = 0;
 let text = "";
+
 while (listaSpesa[i]){
     text += listaSpesa[i] + "<br>";
     i++;
 }
 
 //brutalmente me ne scuso stampo sulla pagina la lista di ogni item singolarmente
-document.getElementById("demo").innerHTML = text;
-*/
+// document.getElementById("demo").innerHTML = text;
+document.getElementById("myBtn").addEventListener("click", function() {
+    myFunction(text);
+  });
 
+  function myFunction(text) {
+    document.getElementById("demo").innerHTML = text;
+  }
